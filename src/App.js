4b4c745eb33af034ee8e66d79504f383b0c73cb6
unknown <dfra0004@student.monash.edu>
2023-01-03 14:07:12 +1100

    fixed display & calculate functions

--- src/App.js
+++ src/App.js
@@ -7,19 +7,15 @@ function App() {
   const [expression, setExpression] = React.useState("");
   const [answer, setAnswer] = React.useState(0);
 
-  const calculate = () => {
-    setAnswer(eval(expression));
-  }
+
 
   const display = (symbol) => {
     setExpression((prev) => prev + symbol);
-    if (expression[expression.length - 1] == "=" && /\d/.test(symbol)) {
-      setExpression(symbol);
-    }
-    else {
-      setExpression(answer + symbol);
-    }
   };
+
+  const calculate = () => {
+    setAnswer(eval(expression));
+  }
 
   const allClear = () => {
     setExpression("");
